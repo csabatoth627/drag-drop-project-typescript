51f1fbae3817d46e7cb7c3cf2409c30f9af75ffb
Csaba <csabi627@gmail.com>
2023-12-13 12:56:39 +0100

    add autobind to submithandler

--- src/app.ts
+++ src/app.ts
@@ -1,27 +1,64 @@
+const AutoBind = (_: any, _2: string, descriptor: PropertyDescriptor) => {
+  const originalMethod = descriptor.value;
+  const adjDescriptor = {
+    configurable: true,
+    get() {
+      const boundFn = originalMethod.bind(this);
+      return boundFn;
+    },
+  };
+  return adjDescriptor;
+};
+
 class ProjectInput {
   templateElement: HTMLTemplateElement;
   hostElement: HTMLDivElement;
   element: HTMLFormElement;
+  titleElementIntput: HTMLInputElement;
+  descriptionElementIntput: HTMLInputElement;
+  numberElementIntput: HTMLInputElement;
 
   constructor() {
     this.templateElement = document.getElementById(
       "project-input"
     )! as HTMLTemplateElement;
     this.hostElement = document.getElementById("app")! as HTMLDivElement;
-    
 
-    const importedNode = document.importNode(this.templateElement.content, true);
-    
+    const importedNode = document.importNode(
+      this.templateElement.content,
+      true
+    );
+
     this.element = importedNode.firstElementChild as HTMLFormElement;
-    
+    this.element.id = "user-input";
+
+    this.titleElementIntput = this.element.querySelector(
+      "#title"
+    )! as HTMLInputElement;
+    this.descriptionElementIntput = this.element.querySelector(
+      "#description"
+    )! as HTMLInputElement;
+    this.numberElementIntput = this.element.querySelector(
+      "#people"
+    )! as HTMLInputElement;
+
+    this.configure();
     this.attach();
   }
 
+  @AutoBind
+  private submitHandler(e: Event) {
+    e.preventDefault();
+    console.log(this.titleElementIntput.value);
+  }
+
+  private configure() {
+    this.element.addEventListener("submit", this.submitHandler);
+  }
+
   private attach() {
-    this.hostElement.insertAdjacentElement('afterbegin', this.element)
+    this.hostElement.insertAdjacentElement("afterbegin", this.element);
   }
 }
 
 const project = new ProjectInput();
-
-
