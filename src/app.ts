cd332cc2d8cf2bc32b428a21e05197fe224aebf8
Csaba <csabi627@gmail.com>
2024-01-09 18:32:21 +0100

    refactor: create project-state.ts

--- src/app.ts
+++ src/app.ts
@@ -1,66 +1,13 @@
 /// <reference path='drag-drop-interfaces.ts' />
 /// <reference path='project-model.ts' />
+/// <reference path='project-state.ts' />
+
 
 
 namespace App {
-  // Project Type
 
 
-  // Manage Project State
-  type Listener<T> = (items: T[]) => void;
-
-  class State<T> {
-    protected listeners: Listener<T>[] = [];
-
-    addListener(listenerFn: Listener<T>) {
-      this.listeners.push(listenerFn);
-    }
-  }
-
-  class ProjectState extends State<Project> {
-    private projects: Project[] = [];
-    private static instance: ProjectState;
-
-    private constructor() {
-      super();
-    }
-
-    static getInstance() {
-      if (this.instance) {
-        return this.instance;
-      }
-      this.instance = new ProjectState();
-      return this.instance;
-    }
-
-    addProjects(title: string, description: string, numOfPeople: number) {
-      const newProject = new Project(
-        Math.random().toString(),
-        title,
-        description,
-        numOfPeople,
-        ProjectStatus.Active
-      );
-      this.projects.push(newProject);
-      this.updateListener();
-    }
-
-    moveProject(projctId: string, newStatus: ProjectStatus) {
-      const project = this.projects.find((prj) => prj.id === projctId);
-      if (project && project.status !== newStatus) {
-        project.status = newStatus;
-        this.updateListener();
-      }
-    }
-
-    private updateListener() {
-      for (const listenerFn of this.listeners) {
-        listenerFn(this.projects.slice());
-      }
-    }
-  }
-
-  const projectState = ProjectState.getInstance();
+ 
 
   // Validation
   interface Validatable {
